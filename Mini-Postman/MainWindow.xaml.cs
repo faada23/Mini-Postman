c8994762df09846e42ab511acc15d64db455bf28
Artsiukh Daniil <artjx_da_22@mf.grsu.by>
2025-11-25 14:43:34 +0300

    Basic mvvm models added, custum Dropdown box style added, MainWindow xaml changed.

--- Mini-Postman/MainWindow.xaml.cs
+++ Mini-Postman/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Mini_Postman.ViewModels;
 
 namespace Mini_Postman;
 
@@ -19,5 +20,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        DataContext = new MainViewModel();
     }
 }
---
+++ Mini-Postman/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Mini_Postman.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{   
+    private string _url;
+    private string _responseText;
+
+    public string Url
+    {
+        get => _url;
+        set => SetField(ref _url, value);
+    }
+    
+    public string ResponseText
+    {
+        get => _responseText;
+        set => SetField(ref _responseText, value);
+    }
+    
+    public ICommand SendRequestCommand { get; }
+
+    public MainViewModel()
+    {
+        _url = "https://jsonplaceholder.typicode.com/posts/1";
+        _responseText = "Waiting for request... ";
+
+        SendRequestCommand = new RelayCommand(OnSendRequest);
+    }
+
+    public void OnSendRequest(object? obj)
+    {
+        ResponseText = $"Manual mode! Request for url {Url}";
+    }
+}
